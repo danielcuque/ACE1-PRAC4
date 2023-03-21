8725c33104d20fa08f4af1bbe4c034b1f57d277a
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2023-03-21 15:53:55 -0600

    feat: macros for print msg

--- main.asm
+++ main.asm
@@ -1,7 +1,20 @@
 ;; MACROS
 
+printMsg macro str
+    mov DX, offset str
+    mov AH, 09h
+    int 21h
+endm
+
+.MODEl small
+.STACK 100h
 .RADIX 16
+colDimension equ 11
+rowDimension equ 24
 .DATA
+
+mainTable DB dup(0)
+
 .CODE
 start:
 main PROC
